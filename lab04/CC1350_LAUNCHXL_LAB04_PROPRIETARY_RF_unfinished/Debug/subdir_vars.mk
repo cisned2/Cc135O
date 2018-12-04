3e4b7c85dc0f8e9a4fa0cc103ca1304459526317
cisned2 <36046655+cisned2@users.noreply.github.com>
2018-12-03 17:02:21 -0800

    f

---
+++ lab04/CC1350_LAUNCHXL_LAB04_PROPRIETARY_RF_unfinished/Debug/subdir_vars.mk
@@ -0,0 +1,71 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../CC1350_LAUNCHXL_TIRTOS.cmd 
+
+C_SRCS += \
+../CC1350_LAUNCHXL.c \
+../CC1350_LAUNCHXL_fxns.c \
+../ccfg.c \
+../main_tirtos.c \
+../rfPacketTx.c \
+../scif.c \
+../scif_framework.c \
+../scif_osal_tirtos.c 
+
+C_DEPS += \
+./CC1350_LAUNCHXL.d \
+./CC1350_LAUNCHXL_fxns.d \
+./ccfg.d \
+./main_tirtos.d \
+./rfPacketTx.d \
+./scif.d \
+./scif_framework.d \
+./scif_osal_tirtos.d 
+
+OBJS += \
+./CC1350_LAUNCHXL.obj \
+./CC1350_LAUNCHXL_fxns.obj \
+./ccfg.obj \
+./main_tirtos.obj \
+./rfPacketTx.obj \
+./scif.obj \
+./scif_framework.obj \
+./scif_osal_tirtos.obj 
+
+OBJS__QUOTED += \
+"CC1350_LAUNCHXL.obj" \
+"CC1350_LAUNCHXL_fxns.obj" \
+"ccfg.obj" \
+"main_tirtos.obj" \
+"rfPacketTx.obj" \
+"scif.obj" \
+"scif_framework.obj" \
+"scif_osal_tirtos.obj" 
+
+C_DEPS__QUOTED += \
+"CC1350_LAUNCHXL.d" \
+"CC1350_LAUNCHXL_fxns.d" \
+"ccfg.d" \
+"main_tirtos.d" \
+"rfPacketTx.d" \
+"scif.d" \
+"scif_framework.d" \
+"scif_osal_tirtos.d" 
+
+C_SRCS__QUOTED += \
+"../CC1350_LAUNCHXL.c" \
+"../CC1350_LAUNCHXL_fxns.c" \
+"../ccfg.c" \
+"../main_tirtos.c" \
+"../rfPacketTx.c" \
+"../scif.c" \
+"../scif_framework.c" \
+"../scif_osal_tirtos.c" 
+
+
